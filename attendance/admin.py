4ed07127062f37c1281eb25072cdbdb5600c55cf
Luis Felipe <lfcvs.rj@gmail.com>
2025-07-07 18:33:42 -0300

    attendance

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import Attendance
 
-# Register your models here.
+@admin.register(Attendance)
+class AttendanceAdmin(admin.ModelAdmin):
+    list_display = ('student', 'classroom', 'date', 'was_present', 'academic_year', 'recorded_by')
+    list_filter = ('academic_year', 'classroom', 'was_present')
+    search_fields = ('student__user__name', 'classroom__name', 'recorded_by__user__name')
+    date_hierarchy = 'date'
--- attendance/models.py
+++ attendance/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+from django.db import models
+
+class Attendance(models.Model):
+    student = models.ForeignKey('accounts.StudentProfile', on_delete=models.PROTECT)
+    classroom = models.ForeignKey('academic.Classroom', on_delete=models.PROTECT)
+    academic_year = models.ForeignKey('academic.AcademicYear', on_delete=models.PROTECT)
+    date = models.DateField()
+    was_present = models.BooleanField(default=True)
+    recorded_by = models.ForeignKey('accounts.TeacherProfile', on_delete=models.SET_NULL, null=True, blank=True)
+
+    def __str__(self):
+        status = "Presente" if self.was_present else "Faltou"
+        return f"{self.student.user.name} - {self.date} - {status}"
